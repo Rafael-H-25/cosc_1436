fc95c2c48b85330b9043765b05f115fcc8f88f87
Rafael Hernandez <rafael.hernandez412@my.tccd.edu>
2026-02-17 21:39:24 -0600

    Fixed the lab 1 issue for locating the file

--- labs/Lab1/Lab1/Lab1.cpp
+++ labs/Lab1/Lab1/Lab1.cpp
@@ -1,21 +1,48 @@
-// Lab1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+/*  
+* Lab 1
+* Rafael Hernandez               // Part 1
+* COSC 1436
+* Spring 2026
+*/
 
 #include <iostream>
+#include <string>
+#include <iomanip>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+void main()
+{       // Working good. Don't Edit it.
+    std::string name;
+    std::cout << "Enter your name: ";
+    std:getline(std::cin, name);
+    std::cout << "Hello " << name << std::endl;
+
+    // Working Correctly, Don't edit unless needed.
+    double labOneGrade, labTwoGrade, labThreeGrade, labFourGrade;       // Part 2
+    std::cout << "Enter your lab 1 grade: ";
+    std::cin >> labOneGrade;
+    std::cout << "Enter your lab 2 grade: ";
+    std::cin >> labTwoGrade;
+    std::cout << "Enter your lab 3 grade: ";
+    std::cin >> labThreeGrade;
+    std::cout << "Enter your lab 4 grade: ";
+    std::cin >> labFourGrade;
+
+    double examGradeOne, examGradeTwo, examGradeThree;
+    std::cout << "Enter your exam 1 grade: ";
+    std::cin >> examGradeOne;
+    std::cout << "Enter your exam 2 grade: ";
+    std::cin >> examGradeTwo;
+    std::cout << "Enter your exam 3 grade: ";
+    std::cin >> examGradeThree;
+
+    // Part 3 works correctly
+    double average;
+    average = (double)(labOneGrade + labTwoGrade + labThreeGrade + labFourGrade) / 4; 
+
+    std::cout << "Average lab grade = "
+        << std::fixed << std::setprecision(2)       // Decimals work, set average as double to fix it
+        << average << std::endl;
+
+
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
-// Hi
