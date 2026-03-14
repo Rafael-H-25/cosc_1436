cfb4decf84bab66e9c010fd072cb92e1bf6f4798
Rafael Hernandez <rafael.hernandez412@my.tccd.edu>
2026-03-14 09:26:29 -0500

    started the code

---
+++ labs/Lab2/Lab2/Lab2.cpp
@@ -0,0 +1,24 @@
+/*
+*   Lab 2
+*   Rafael Hernandez
+*   COSC 1436
+*   Spring 2026
+*/
+
+#include <iostream>
+#include <string>
+#include <iomanip>
+struct loan
+{
+    std::string initial
+
+};
+
+
+void main()
+{
+    std::cout << "Enter initial loan amount: ";
+
+    while (initial.loanAmount < 1 || initial.loanAmount > 10000)
+    std::cout << "Hello World!\n";
+}
