6a8ec8c06014f24c38edc1cd2b658ec35d68f068
Rafael Hernandez <rafael.hernandez412@my.tccd.edu>
2026-04-21 12:15:52 -0500

    Lab 4 done

--- labs/Lab4/Lab 4/Lab 4.cpp
+++ labs/Lab4/Lab 4/Lab 4.cpp
@@ -1,64 +1,5 @@
-// Lab 4
-//
-//
-//Use use C++ arrays, Only use C arrays shown in class
-// User input: expecting user input validation
-// 
-// Break up code in functions
-// Functions must be documented
-// Avoid prototypes or foward references.
-// Order your  functions to not need them.
-// Don't assume the size of the array.
-// Stored is where you are declaring it.
-// Don't use global variables.
-// 
-// We use a header function.
-// 
-// Story 2:
-// number are greater than 0
-// create an array that can store up to 100 numbers
-// and provide the user to prompt a number.
-// The user can leave if put 0.
-// 0 is not a part of the array values stored.
-// If user inputs -1 or lower error.
-// If limit of 100, they get out of it.
-// Know the number of values entered. Return that to the function.
-// Make sure the main prgram is gonna need the array and delclare it inside of main for an array.
-// 
-// 
-// Story 3:
-// After the user data collected.
-// "quit" is there
-// We can use 123 or letters.
-// Put it in a function to call the function when the user asks for it.
-// 
-// Story 4:
-// add an option of the largest value in the array, add logic, and call the function for the largest value for the array into the array.
-//  You need to calculate to the array values not upfront.
-//  Display the values to the user and to chose the next option.
-//  
-//  Story 5:  smallest value. dont use 0. the smallest should be 1.
-//
-// Story 6:
-// Sum up all values in array. 
-// if did 123456. We do 6 of them.
-// 
-// Story 7: Calculating the mean or average of the values.
-// Sum array values and divide the 
-// Number divided by the number they asked, they said 4, we divide by four.
-// 4 decimal places.
-// 
-// Story 8: Show the values, just the values enter, if done more than 10, we only show 10 values per line.
-// Story 9: Hardest potentially
-// Add support to new values to the array without overighting it.
-// All rules must be positive 1-100, 0 is out , -# means error.
-// Add a new menu item.
-// Once enter the new value, it will corectly done.
-// 
-//
-
 /*
-*   Lab 3
+*   Lab 4
 *   Rafael Hernandez
 *   COSC 1436
 *   Spring 2026
@@ -66,7 +7,6 @@
 
 #include <iostream>
 #include <iomanip>
-#include <string>
 
 const int MAX_SIZE = 100;
 
